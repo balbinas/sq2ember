66af4d8441cd071a4317cf28cf7bc05897befa6a
Balbina <balbinasantana@gmail.com>
2020-04-13 13:37:19 -0500

    tracked imprt

--- app/controllers/layout.js
+++ app/controllers/layout.js
@@ -2,7 +2,7 @@ import Controller from '@ember/controller';
 import { action } from "@ember/object";
 import fetch, { Headers } from 'fetch';
 import ENV from 'squared-up/config/environment';
-
+import { tracked } from '@glimmer/tracking';
 
 export default class LayoutController extends Controller {
 
